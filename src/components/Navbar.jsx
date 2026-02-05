1214f3dee76043b81309f413886f6ef4fae1966a
FernandoNLopez <lopezfer1998@outlook.com>
2026-02-05 12:23:59 -0300

    First Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import dayjs from "dayjs";
+
+import {navIcons, navLinks} from "#constants/index.js";
+
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <img src="/images/logo.svg" alt="logo" />
+                <p className="font-bold">Fernando's IOS Style</p>
+
+                <ul>
+                    {navLinks.map(({ id, name }) => ( //  ( means it'll return whatever is within it.
+                        <li key={id}>
+                            <p> {name} </p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            <div>
+                <ul>
+                    {navIcons.map(({ id, img }) => (
+                        <li key={id}>
+                            <img
+                                src={img}
+                                alt={`icon-${id}`}
+                                className="icon-hover"
+                            />
+                        </li>
+                    ))}
+                </ul>
+
+                <time>
+                    {dayjs().format('YYYY-MM-DD HH:mm')}
+                </time>
+            </div>
+        </nav>
+    )
+}
+export default Navbar
